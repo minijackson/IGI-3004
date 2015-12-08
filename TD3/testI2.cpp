6aed740bd5425d9f4c0517e17413508ff712b4cc
Minijackson <minijackson@riseup.net>
2015-12-08 10:48:20 +0100

    Almost adapting to new TD3 subject

--- TD3/testI2.cpp
+++ TD3/testI2.cpp
@@ -12,7 +12,13 @@ constexpr size_t const TAILLEBUF            = 255;
 constexpr char const SECRET_INDICATOR[]     = "secret: ";
 constexpr size_t const SECRET_INDICATOR_LEN = 8;
 
-int main() {
+int main(int argc, char* argv[]) {
+
+	if(argc != 3) {
+		std::cout << "Usage: " << argv[0] << " source destination" << std::endl;
+		return EINVAL;
+	}
+
 	try {
 		Pipe myPipe;
 		pid_t pid = fork();
@@ -21,27 +27,32 @@ int main() {
 			std::perror("fork() a échoué");
 			exit(errno);
 		} else if(pid == 0) {
+			try {
+				OFile destinationFile(argv[2], TAILLEBUF);
 
-			char receivedMessage[TAILLEBUF];
-			myPipe >> receivedMessage;
-
-			// Character 0x04 is EOT (end of transmission)
-			while(receivedMessage[0] != 0x04) {
-				std::cout << receivedMessage;
+				char receivedMessage[TAILLEBUF];
 				myPipe >> receivedMessage;
-			}
 
+				// Character 0x04 is EOT (end of transmission)
+				while(receivedMessage[0] != 0x04) {
+					destinationFile << receivedMessage;
+					myPipe >> receivedMessage;
+				}
+			} catch(std::ios_base::failure const& e) {
+				std::cerr << "Error: " << e.what() << std::endl;
+				return e.code().value();
+			}
 		} else {
 
 			// Character 0x04 is EOT (end of transmission)
 			char const endMessage[] = {0x04, '\0'};
 
 			try {
-				IFile myFile("almostSecret.txt", TAILLEBUF);
+				IFile sourceFile(argv[1], TAILLEBUF);
 				char messageToTransmit[TAILLEBUF];
 
-				while(!myFile.hasEnded()) {
-					myFile >> messageToTransmit;
+				while(!sourceFile.hasEnded()) {
+					sourceFile >> messageToTransmit;
 					if(std::strncmp(messageToTransmit, SECRET_INDICATOR, SECRET_INDICATOR_LEN)) {
 						myPipe << messageToTransmit;
 					}
