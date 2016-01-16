fd739f18ebc346f31ab90c36abc3e101a6ea3ae4
Minijackson <minijackson@riseup.net>
2016-01-16 23:31:16 +0100

    Passing semaphores by reference in thread functions

--- src/TD4/testIIb.cpp
+++ src/TD4/testIIb.cpp
@@ -3,19 +3,17 @@
 #include "semaphore.hpp"
 #include "thread.hpp"
 
-Semaphore sem[3];
-
-void thread_print(size_t i) {
-	int const waitingSemaphoreID = i - 1, releasingSemaphoreID = i % 3;
-
+void thread_print(size_t i, Semaphore& waitingSemaphore, Semaphore& releasingSemaphore) {
 	for(int j = 0; j < 10; j++) {
-		sem[waitingSemaphoreID].wait();
+		waitingSemaphore.wait();
 		std::cout << "Affichage " << j + 1 << " du thread " << i << std::endl;
-		sem[releasingSemaphoreID].post();
+		releasingSemaphore.post();
 	}
 }
 
 int main() {
+	Semaphore sem[3];
+
 	for(int i = 0; i < 3; ++i) {
 		sem[i] = Semaphore(0);
 	}
@@ -24,7 +22,9 @@ int main() {
 	Thread threads[3];
 
 	for(size_t i = 1; i <= 3; ++i) {
-		threads[i - 1].start(thread_print, i);
+		int const waitingSemaphoreID = i - 1, releasingSemaphoreID = i % 3;
+		threads[i - 1].start(thread_print, i, std::ref(sem[waitingSemaphoreID]),
+		                     std::ref(sem[releasingSemaphoreID]));
 	}
 
 	for(int i = 0; i < 3; ++i) {
